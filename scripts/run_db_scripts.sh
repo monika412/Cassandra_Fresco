2baab52a7793f1ea316e640a023608cf41543bfd
Kanishk Agarwal <kanishkagarwaal@gmail.com>
2018-09-04 11:13:05 +0100

    Update run_db_scripts.sh

--- scripts/run_db_scripts.sh
+++ scripts/run_db_scripts.sh
@@ -11,6 +11,6 @@ CREATE KEYSPACE fresco_seg  WITH REPLICATION = { 'class' : 'SimpleStrategy','rep
 
 create table fresco_seg.customer_seg (party_id text, fresco13_mseg text,fresco13_seg text,fresco13_sseg text,match_flag list<text> ,PRIMARY KEY(party_id));
 
-COPY fresco_seg.customer_seg (party_id,fresco13_mseg,fresco13_seg,fresco13_sseg,match_flag) FROM '/tmp/cass_DB/Cassandra_Fresco/data/DatasetGenerated.csv' WITH DELIMITER=',' AND HEADER = TRUE ;
+COPY fresco_seg.customer_seg (party_id,fresco13_mseg,fresco13_seg,fresco13_sseg,match_flag) FROM '/tmp/cass_DB/Cassandra_Fresco/data/DatasetGenerated.csv' WITH DELIMITER=',' AND HEADER = TRUE 
 
 "
