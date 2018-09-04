b5fa973f3510ef28f3659dc45fd542b0dfbfb581
Kanishk Agarwal <kanishkagarwaal@gmail.com>
2018-09-04 16:07:42 +0100

    Update run_db_scripts.sh
    Adding tail to file

--- scripts/run_db_scripts.sh
+++ scripts/run_db_scripts.sh
@@ -14,5 +14,5 @@ create table fresco_seg.customer_seg (party_id text, fresco13_mseg text,fresco13
 COPY fresco_seg.customer_seg (party_id,fresco13_mseg,fresco13_seg,fresco13_sseg,match_flag) FROM '/tmp/cass_DB/Cassandra_Fresco/data/DatasetGenerated.csv' WITH DELIMITER=',' AND HEADER = TRUE 
 
 "
-
-cat
+touch tail.log
+tail -f tail.log
